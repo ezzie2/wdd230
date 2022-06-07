f0e7c974d99bf4b6e9d3142fc46a05b1a2dcd4e3
ezzie2 <ver12003@byui.edu>
2022-06-06 23:00:52 -0600

    discovery images2

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -6,3 +6,22 @@ document.getElementById('currenttime').textContent = new Date().toLocaleDateStri
 
 const date = {weekday: "long", day: 'numeric', month: "long", year: "numeric"};
 document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', date);
+
+//locale storage and visits script
+
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visit-ls"));
+
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+localStorage.setItem("visit-ls", numVisits);
+
+todayDisplay.textContent = Date.now();
