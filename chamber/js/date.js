3031526a839eedfb1dd8840fa312d07cf3dca7b9
ezzie2 <ver12003@byui.edu>
2022-06-14 11:17:39 -0600

    Number of visits javascript code corrected

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -9,19 +9,19 @@ document.getElementById('date').textContent = new Date().toLocaleDateString('en-
 
 //locale storage and visits script
 
-const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("visit-ls"));
+let todayDisplay = Number(window.localStorage.getItem("todayDisplay"));
 
+const timeBetween = Math.round((Date.now() - todayDisplay) / (1000 * 60 * 6 * 24));
 
 if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+	visitsDisplay.textContent = timeBetween;
 } else {
 	visitsDisplay.textContent = `This is your first visit!`;
 }
 
 numVisits++;
 localStorage.setItem("visit-ls", numVisits);
-
-todayDisplay.textContent = Date.now();
+localStorage.setItem("todayDisplay", Date.now());
