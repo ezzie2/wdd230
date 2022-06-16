131791abe6242fd911be63ff0e8d659f85f7bbc6
ezzie2 <ver12003@byui.edu>
2022-06-15 23:09:48 -0600

    week 8 final changes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,9 +1,23 @@
+// toggle Menu //
+
 function toggleMenu() {
     document.getElementById("navigationMenu").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
+
+
+
+// Date on Header //
+const y = {year: 'numeric'}
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', y);
+
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('currenttime').textContent = new Date().toLocaleDateString('en-US',options);
+
+const date = {weekday: "long", day: 'numeric', month: "long", year: "numeric"};
+document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', date);
 
 
 
