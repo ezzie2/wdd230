215f9cea13234c39dabc47c3673bba058f52260d
ezzie2 <ver12003@byui.edu>
2022-07-21 03:37:27 -0600

    changes

--- hotels/js/script.js
+++ hotels/js/script.js
@@ -8,3 +8,14 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
+
+
+// Date //
+const y = {year: 'numeric'}
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', y);
+
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('currenttime').textContent = new Date().toLocaleDateString('en-US',options);
+
+const date = {weekday: "long", day: 'numeric', month: "long", year: "numeric"};
+document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', date);
